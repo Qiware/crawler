e609d771ef7958b7e867d1003b7e1367cbd79dbb
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2016-09-30 22:54:49 +0800

    [EXEC]: 将公共组建加入编译框架

---
+++ bin/spider.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ulimit -c unlimited
+. ./prepare.sh
+
+export LD_LIBRARY_PATH='/usr/lib/x86_64-linux-gnu:../lib'
+
+# 启动服务
+./crawler -l trace -c ../conf/crawler.xml -d   # 爬虫服务
+./filter -l trace -c ../conf/filter.xml -d     # 爬虫过滤服务
+
+#./watch.sh     # 监控进程状态
